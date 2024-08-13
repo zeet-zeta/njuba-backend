2fbf1143cbb88a261e08eceb45f85600154b650c
zeet <231250160@smail.nju.edu.cn>
2024-08-13 08:51:19 +0800

    加入test

--- test/controller/category.test.ts
+++ test/controller/category.test.ts
@@ -1,20 +1,21 @@
 import { createApp, close, createHttpRequest } from '@midwayjs/mock';
 import { Framework } from '@midwayjs/koa';
 
-describe('test/controller/home.test.ts', () => {
+describe('test/controller/category.test.ts', () => {
 
-  it('should POST /api/get_user', async () => {
+  it('should GET /category/get', async () => {
     // create app
     const app = await createApp<Framework>();
 
     // make request
-    const result = await createHttpRequest(app).get('/api/get_user').query({ uid: 123 });
+    const result = await createHttpRequest(app).get('/category/get');
 
     // use expect by jest
     expect(result.status).toBe(200);
-    expect(result.body.message).toBe('OK');
+    expect(result.text).toBe('["生活","科技","水贴","学习","校园"]');
 
     // close app
     await close(app);
   });
+
 });
